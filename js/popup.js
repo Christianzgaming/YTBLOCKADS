a4e738ba8abf19db234717726b8ff95e923541b6
Christian Gaming <82629444+Christianzgaming@users.noreply.github.com>
2024-07-02 14:01:17 +0800

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 const extpay = ExtPay('fadblock')
 
 function getArtefacts() {
-    fetch('https://raw.githubusercontent.com/Christianzgaming/fadblock/master/fadblock.json', { 
+    fetch('https://raw.githubusercontent.com/Christianzgaming/YTBLOCKADS/main/fadblock.json', { 
       method: 'GET'
     })
     .then(function(response) { return response.json(); })
